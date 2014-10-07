88725631472bb603a1a8f83ab61fc62d79d1b93b
Troy Goode <troygoode@gmail.com>
2014-10-07 12:41:42 -0700

    refactored routes into separate files

--- routes/map.js
+++ routes/map.js
@@ -2,29 +2,6 @@ var express = require('express'),
   sfdc = require('../sfdc'),
   router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('login', {
-    authUri: sfdc.getAuthUri()
-  });
-});
-
-router.get('/oauth/_callback', function (req, res, next) {
-  sfdc.authenticate({code: req.query.code}, function (err, oauth) {
-    if (err) { return next(err); }
-    req.session = oauth;
-    res.redirect('/map');
-  });
-});
-
-// only authenticated users beyond this point
-router.use(function (req, res, next) {
-  if (req.session && req.session.access_token) {
-    next();
-  } else {
-    res.redirect('/');
-  }
-});
-
 /* GET map page. */
 router.get('/map', function(req, res) {
   res.render('map', {
