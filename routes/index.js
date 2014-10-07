35f8c3e07ec49586d9f4e3f0ff2644d2ceca5d6a
Troy Goode <troygoode@gmail.com>
2014-10-07 11:07:04 -0700

    we have maps\!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { access_token: process.env.ACCESS_TOKEN });
 });
 
 module.exports = router;
