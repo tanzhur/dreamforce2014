343241253f1b37c8284e6e9c4e38c55df2287983
Troy Goode <troygoode@gmail.com>
2014-10-07 11:32:53 -0700

    add geocoding

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,11 @@
 L.mapbox.accessToken = window.mapboxAccessToken;
 
-var map = L.map('map')
-    .addLayer(L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + window.mapboxMapId + '/{z}/{x}/{y}.png'))
-    .setView([37.789178, -122.409690], 14);
+var geocoder = L.mapbox.geocoder('mapbox.places-v1'),
+  mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + window.mapboxMapId + '/{z}/{x}/{y}.png'),
+  map = L.map('map').addLayer(mapboxTiles);
+
+geocoder.query('575 Sutter Street, San Francisco, CA', function (err, data) {
+  if (err) { return console.error(err); }
+  map.setView([data.latlng[0], data.latlng[1]], 14);
+});
 
