8916b7b5f5f4926367e135cdbc152f10b46d63af
Troy Goode <troygoode@gmail.com>
2014-10-07 11:19:34 -0700

    now using leaflet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { mapbox_access_token: process.env.MAPBOX_ACCESS_TOKEN });
+  res.render('index', {
+    mapboxAccessToken: process.env.MAPBOX_ACCESS_TOKEN,
+    mapboxMapId: process.env.MAPBOX_MAP_ID
+  });
 });
 
 module.exports = router;
+
