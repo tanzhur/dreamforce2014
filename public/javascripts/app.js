6cc0fd36e8cfaa6d663ad03ae810dd109c5f9164
Troy Goode <troygoode@gmail.com>
2014-10-07 11:23:12 -0700

    zoomed over to SFBA

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,6 @@
 L.mapbox.accessToken = window.mapboxAccessToken;
-// Replace 'examples.map-i87786ca' with your map id.
-var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + window.mapboxMapId + '/{z}/{x}/{y}.png', {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-});
 
 var map = L.map('map')
-    .addLayer(mapboxTiles)
-    .setView([42.3610, -71.0587], 15);
+    .addLayer(L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + window.mapboxMapId + '/{z}/{x}/{y}.png'))
+    .setView([37.789178, -122.409690], 14);
 
